3faa5ff375c9d3d93215f7c6fc7de35cf32969e4
ahmadrama0999 <ahmadrama0999@gmail.com>
2022-07-30 19:43:13 +0100

    Create initial project

---
+++ HamiltonAssessmentTask/HamiltonAssessmentTask/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  HamiltonAssessmentTask
+//
+//  Created by Ramin Akhmad on 30.07.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = ViewController()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
---
+++ HamiltonAssessmentTask/HamiltonAssessmentTask/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HamiltonAssessmentTask
+//
+//  Created by Ramin Akhmad on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
